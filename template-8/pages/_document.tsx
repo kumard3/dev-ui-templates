cbfb29567d1d0fd93fe1114d1c91d301730508a9
kumard3 <kumardeepanshu157200@gmail.com>
2022-04-16 19:53:38 +0530

    updated

--- template-8/pages/_document.tsx
+++ template-8/pages/_document.tsx
@@ -18,6 +18,79 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Kumar Deepanshu</title>
+          <meta
+            name="description"
+            content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
+          />
+          <meta
+            name="viewport"
+            content="width=device-width,minimum-scale=1,initial-scale=1"
+          />
+          <meta property="og:type" content="website" />
+          <meta name="og:title" property="og:title" content="Kumar Deepanshu" />
+          <meta
+            name="og:description"
+            property="og:description"
+            content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
+          />
+          <meta
+            property="og:site_name"
+            content="Kumar deepanshu Personal Portfolio"
+          />
+
+          <meta name="twitter:card" content="It is my personal portfolio" />
+          <meta name="twitter:title" content="Kumar Deepanshu" />
+          <meta
+            name="twitter:description"
+            content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
+          />
+          <meta name="twitter:site" content="@propernoun" />
+          <meta name="twitter:creator" content="@propernounco" />
+
+          <meta
+            property="og:image"
+            content="https://portfolio-templte.pages.dev/ogImage.png"
+          />
+          <meta
+            name="twitter:image"
+            content="https://portfolio-templte.pages.dev/ogImage.png"
+          />
+
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:url"
+            content="https://portfolio-templte.pages.dev/"
+          />
+          <meta property="og:title" content="Kumar Deepanshu" />
+          <meta
+            property="og:description"
+            content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
+          />
+          <meta
+            property="og:image"
+            content="https://portfolio-templte.pages.dev/ogImage.png"
+          />
+          <meta property="og:site_name" content="Kumar Deepanshu" />
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta
+            property="twitter:url"
+            content="https://portfolio-templte.pages.dev/"
+          />
+          <meta property="twitter:title" content="Kumar Deepanshu" />
+          <meta
+            property="twitter:description"
+            content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
+          />
+          <meta
+            property="twitter:image"
+            content="https://portfolio-templte.pages.dev/ogImage.png"
+          />
+          <meta name="robots" content="Index" />
+          <link rel="manifest" href="/manifest.json" />
+
           <meta charSet="utf-8" />
         </Head>
         <body className=" bg-white dark:bg-gradient-to-r dark:from-[#1F2937]  dark:to-[#111827] min-h-screen w-full relative text-gray-600 dark:text-white ">
