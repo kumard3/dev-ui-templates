2f663613e20562c12f8905d8f63fee4edad76ba3
kumard3 <kumardeepanshu157200@gmail.com>
2022-04-16 21:40:38 +0530

    updated

--- template-8/pages/about.tsx
+++ template-8/pages/about.tsx
@@ -3,20 +3,25 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="container mx-auto flex flex-col items-start px-5 mt-6 max-w-5xl min-h-[140vh] md:min-h-screen ">
+    <div className="container mx-auto flex flex-col items-start px-5 mt-6 max-w-5xl min-h-[182vh] xs:min-h-[170vh] sm:min-h-screen ">
       <div className="bg-[#60A5FA] h-[120px] top-36 right-20 absolute w-[120px] rounded-full blur-[90px] filter "></div>
       <div className="bg-[#60A5FA] h-[120px] top-[200px] right-[200px] absolute w-[120px] rounded-full blur-[90px] filter "></div>
-      <h1 className="SpaceGroteskBold text-[64px]">About Me 👨‍</h1>
-      <p className="SpaceGroteskRegular text-[24px]">
+      <h1 className="SpaceGroteskBold text-[50px] sm:text-[64px]">
+        About Me 👨‍
+      </h1>
+      <p className="SpaceGroteskRegular text-[20px] sm:text-[24px] ">
         A brief intro to who i am and how i do what i do:
       </p>
-      <h1 className="SpaceGroteskRegular py-5 text-4xl ">
-        I am <span className="text-6xl SpaceGroteskBold ">Kumar Deepanshu</span>
+      <h1 className="SpaceGroteskRegular py-5 text-2xl sm:text-4xl ">
+        I am{" "}
+        <span className=" text-4xl sm:text-6xl SpaceGroteskBold ">
+          Kumar Deepanshu
+        </span>
       </h1>
       <div className="bg-[#60A5FA] h-[120px] top-[30rem] left-[5px] absolute w-[120px] rounded-full blur-[90px] filter "></div>
 
-      <div className="SpaceGroteskRegular text-[24px] max-w-5xl mt-3">
-        <p className="SpaceGroteskRegular text-[24px] py-5">
+      <div className="SpaceGroteskRegular text-[20px] sm:text-[24px]  max-w-5xl mt-3">
+        <p className="SpaceGroteskRegular text-[20px] sm:text-[24px]  py-5">
           An enthusiastic front-end software engineer with more than 4 years of
           experience in the field. during the last four years, from front-end to
           mobile to desktop to back-end I have experience of developing a lot of
@@ -25,7 +30,7 @@ export default function About() {
           product owners, developers, and designers to achieve successful
           implementations.
         </p>
-        <p className="SpaceGroteskRegular text-[24px] py-5">
+        <p className="SpaceGroteskRegular text-[20px] sm:text-[24px]  py-5">
           I believe i've got the right skills and technologies to take your
           online presence on the level it truly deserves. Your website should be
           your no1 sales agent and its works 24/7, Get a quote now that's best
--- template-8/tailwind.config.js
+++ template-8/tailwind.config.js
@@ -1,11 +1,31 @@
 module.exports = {
-  darkMode: 'class',
-  purge: { enabled: true, content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],},
+  darkMode: "class",
+  purge: {
+    enabled: true,
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+  },
   theme: {
+    screens: {
+      xs: "400px",
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [],
-}
+};
