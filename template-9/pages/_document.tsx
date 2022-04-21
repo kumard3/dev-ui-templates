aa70f531c90b7ff5bcfe2277e6874f5fe4d6d868
kumard3 <kumardeepanshu157200@gmail.com>
2022-04-21 23:06:38 +0530

    updated

--- template-9/pages/_document.tsx
+++ template-9/pages/_document.tsx
@@ -17,75 +17,75 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>Kumar Deepanshu</title>
+          <title>NFT Page</title>
           <meta
             name="description"
-            content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
+            content="A Nft Landing Page"
           />
           <meta
             name="viewport"
             content="width=device-width,minimum-scale=1,initial-scale=1"
           />
           <meta property="og:type" content="website" />
-          <meta name="og:title" property="og:title" content="Kumar Deepanshu" />
+          <meta name="og:title" property="og:title" content="NFT Landing Page" />
           <meta
             name="og:description"
             property="og:description"
-            content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
+            content="A Nft Landing Page"
           />
           <meta
             property="og:site_name"
-            content="Kumar deepanshu Personal Portfolio"
+            content="NFT Landing Page"
           />
 
           <meta name="twitter:card" content="It is my personal portfolio" />
-          <meta name="twitter:title" content="Kumar Deepanshu" />
+          <meta name="twitter:title" content="NFT Landing Page" />
           <meta
             name="twitter:description"
-            content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
+            content="A Nft Landing Page"
           />
           <meta name="twitter:site" content="@propernoun" />
           <meta name="twitter:creator" content="@propernounco" />
 
           <meta
             property="og:image"
-            content="https://portfolio-templte.pages.dev/ogImage.png"
+            content="https://dev-ui-templates.pages.dev/ogImage.png"
           />
           <meta
             name="twitter:image"
-            content="https://portfolio-templte.pages.dev/ogImage.png"
+            content="https://dev-ui-templates.pages.dev/ogImage.png"
           />
 
           {/* Open Graph / Facebook */}
           <meta property="og:type" content="website" />
           <meta
             property="og:url"
-            content="https://portfolio-templte.pages.dev/"
+            content="https://dev-ui-templates.pages.dev/"
           />
-          <meta property="og:title" content="Kumar Deepanshu" />
+          <meta property="og:title" content="NFT Landing Page" />
           <meta
             property="og:description"
-            content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
+            content="A Nft Landing Page"
           />
           <meta
             property="og:image"
-            content="https://portfolio-templte.pages.dev/ogImage.png"
+            content="https://dev-ui-templates.pages.dev/ogImage.png"
           />
-          <meta property="og:site_name" content="Kumar Deepanshu" />
+          <meta property="og:site_name" content="NFT Landing Page" />
           {/* Twitter */}
           <meta property="twitter:card" content="summary_large_image" />
           <meta
             property="twitter:url"
-            content="https://portfolio-templte.pages.dev/"
+            content="https://dev-ui-templates.pages.dev/"
           />
-          <meta property="twitter:title" content="Kumar Deepanshu" />
+          <meta property="twitter:title" content="NFT Landing Page" />
           <meta
             property="twitter:description"
-            content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
+            content="A Nft Landing Page"
           />
           <meta
             property="twitter:image"
-            content="https://portfolio-templte.pages.dev/ogImage.png"
+            content="https://dev-ui-templates.pages.dev/ogImage.png"
           />
           <meta name="robots" content="Index" />
           <link rel="manifest" href="/manifest.json" />
